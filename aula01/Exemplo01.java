38224a5f655a50ce1ed9e78e46283c082cdf0270
Gustavo Brand <41551014+gustavobrand@users.noreply.github.com>
2023-02-14 22:20:28 -0300

    Create Exemplo01.java

---
+++ aula01/Exemplo01.java
@@ -0,0 +1,27 @@
+package aula01;
+
+public class Exemplo01 {
+
+	public static void main(String[] args) {
+//		int a = 10;
+//		System.out.println("Teste 1.0");
+//		
+//		if (a > 15) {
+//			System.out.println("Teste 1.1");
+//			System.out.println("Teste 1.1.1");
+//		}
+//		
+//		System.out.println("Teste 1.2");
+		
+//		int i=10;
+//		while (i>0) {
+//			System.out.println("Valor do i: " + i);
+//			i--;
+//		}
+		
+		for (int i=0; i<10; i++) {
+			System.out.println("Valor do i: " + i);
+		}
+	}
+
+}
